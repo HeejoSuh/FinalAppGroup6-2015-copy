ced564433290b6ee8e5589a71d982b8c94e308b5
alessandroI at codea-scm <alessandroI@codea-scm.aws.mapote.com>
2015-11-23 11:03:10 -0500

    checkpoint commit on behalf of alessandroI

---
+++ tabs/MainScene.lua
@@ -0,0 +1,25 @@
+-- Lesson6
+-- Created by: Alessandro Iaderosa
+-- Created on: Nov - 2015
+-- Created for: ICS2O
+-- This is the second scene
+
+MainScene = class()
+
+
+function MainScene:init(x)
+  
+end
+
+function MainScene:draw()
+    -- Codea does not automatically call this method
+
+    background(0, 30, 255, 255)
+
+
+end
+
+
+function MainScene:touched(touch)
+    -- Codea does not automatically call this method
+end
---
+++ tabs/SplashScreenScene.lua
@@ -0,0 +1,37 @@
+-- Lesson6
+-- Created by: Alessandro Iaderosa
+-- Created on: Nov - 2015
+-- Created for: ICS2O
+-- This is the first scene
+
+SplashScreenScene = class()
+
+-- global to this file
+local moveForwardButton
+
+function SplashScreenScene:init(x)
+   
+    --  sprite("Cargo Bot:Command Right")
+    moveForwardButton = Button("Cargo Bot:Command Right", vec2(WIDTH/2, HEIGHT/2))
+           
+end
+
+function SplashScreenScene:draw()
+    -- Codea does not automatically call this method
+    
+    background(255, 0, 7, 255)
+    moveForwardButton:draw()
+end
+
+function SplashScreenScene:touched(touch)
+    -- Codea does not automatically call this method
+    
+    moveForwardButton:touched(touch)
+    if(moveForwardButton.selected == true) then 
+    Scene.Change("play")
+    end
+
+
+
+
+end
